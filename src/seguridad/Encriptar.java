c6eb4416117c4de24e00899e3c8f0c2398a9e9b7
nicoyarce <nicoyarce@gmail.com>
2017-11-14 12:05:13 -0300

    Agregado vector iv para cbc

--- src/seguridad/Encriptar.java
+++ src/seguridad/Encriptar.java
@@ -1,69 +1,59 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
- * Ejemplo de encriptado y desencriptado con algoritmo AES.
- * Se apoya en RSAEncrypt.java para salvar en fichero
- * o recuperar la clave de encriptacion.
- * 
+ * Ejemplo de encriptado y desencriptado con algoritmo AES. Se apoya en
+ * RSAEncrypt.java para salvar en fichero o recuperar la clave de encriptacion.
+ *
  * @author Chuidiang
  *
  */
 public class Encriptar {
 
-  public static void main(String[] args) throws Exception {
-    // Generamos una clave de 128 bits adecuada para AES
-    KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-    keyGenerator.init(128);
-    Key key = keyGenerator.generateKey();
+    public static void main(String[] args) throws Exception {        
+        File textoCifrado = new File("texto.txt");        
 
-    File archivoLlave = new File("llave.txt");
-    File textoCifrado = new File("texto.txt");
+        // Texto a encriptar
+        String texto = "Hola1";
+        System.out.println("Texto plano:" + texto);
 
-    // Texto a encriptar
-    String texto = "Hola1";
-    System.out.println("Texto plano:"+texto);
-      
-    // Se obtiene un cifrador AES
-    Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        // Generamos llave de sesion AES de 128 bits
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        Key key = keyGenerator.generateKey();
 
-    // Se inicializa para encriptacion y se encripta el texto,
-    // que debemos pasar como bytes.
-    aes.init(Cipher.ENCRYPT_MODE, key);
-    byte[] encriptado = aes.doFinal(texto.getBytes());
+        // Se obtiene un cifrador AES que usa CBC
+        Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecureRandom randomSecureRandom = SecureRandom.getInstance("SHA1PRNG");
+        byte[] iv = new byte[aes.getBlockSize()];
+        randomSecureRandom.nextBytes(iv);
+        IvParameterSpec ivParams = new IvParameterSpec(iv);
+        
+        //Generacion de hash del mensaje
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(texto.getBytes());
+        byte[] hash = md.digest();
+        
 
-    // Se escribe byte a byte en hexadecimal el texto
-    // encriptado para ver su pinta.
-    /*System.out.print("Texto encriptado:");
-    for (byte b : encriptado) {
-       System.out.print(Integer.toHexString(0xFF & b));
+        // Se inicializa para encriptacion y se encripta el texto,
+        // que debemos pasar como bytes.
+        aes.init(Cipher.ENCRYPT_MODE, key, ivParams);
+        byte[] encriptado = aes.doFinal(texto.getBytes());           
+        
+        FileOutputStream alArchivo = new FileOutputStream(textoCifrado);
+        System.out.println("Escribiendo Datos");
+        alArchivo.write(encriptado);   
     }
-    System.out.println();*/
-
-    System.out.println("Abriendo archivo llave: "+archivoLlave);
-
-    byte[] fileData = key.getEncoded();
-
-    FileOutputStream alArchivo = new FileOutputStream(archivoLlave);
-    System.out.println("Escribiendo Datos");
-    alArchivo.write(fileData);
-
-    ////////////////////////////////////////////////////////////////////
-    
-    System.out.println("Abriendo archivo texto cifrado: "+textoCifrado);
-
-    fileData = encriptado;
-
-    alArchivo = new FileOutputStream(textoCifrado);
-    System.out.println("Escribiendo Datos");
-    alArchivo.write(fileData);     
-  }
 }
